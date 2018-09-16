ed9aa89099e599973da85276ce0094360e33a76d
Chanyong Shim <chanyong.shim@gmail.com>
2018-09-16 22:13:50 +0900

    add: Storyboad Enumeration 추가, (Storyboard 관리)

---
+++ Movement/Library/Storyboard.swift
@@ -0,0 +1,29 @@
+//
+//  Storyboard.swift
+//  Movement
+//
+//  Created by CMR on 16/09/2018.
+//  Copyright © 2018 lyle. All rights reserved.
+//
+
+import UIKit
+
+public enum Storyboard: String {
+    case Main
+    case Ingredients
+    
+    public func instantiate<VC: UIViewController>(_ viewController: VC.Type,
+                                                  inBundle bundle: Bundle = .framework) -> VC {
+        
+        guard let vc = UIStoryboard(name: self.rawValue, bundle: Bundle(identifier: bundle.bundleIdentifier!))
+            .instantiateViewController(withIdentifier: VC.storyboardIdentifier) as? VC else {
+                fatalError("Couldn't instantiate \(VC.storyboardIdentifier) from \(self.rawValue)")
+        }
+        return vc
+    }
+}
+extension UIViewController {
+    public static var storyboardIdentifier: String {
+        return self.description().components(separatedBy: ".").dropFirst().joined(separator: ".")
+    }
+}
