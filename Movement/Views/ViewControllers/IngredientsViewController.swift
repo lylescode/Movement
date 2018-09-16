881ca09699feff4e42d04416c963d252d0e2ad33
Chanyong Shim <chanyong.shim@gmail.com>
2018-09-16 22:12:20 +0900

    add: Ingredients.storyboard, IngredientsViewController

---
+++ Movement/Views/ViewControllers/IngredientsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IngredientsViewController.swift
+//  Movement
+//
+//  Created by CMR on 16/09/2018.
+//  Copyright © 2018 lyle. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
